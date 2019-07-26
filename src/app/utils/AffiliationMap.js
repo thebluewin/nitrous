3bd7c00f30118d5cb7800c6a12f0ba1cebed3ace
Neo-xian <mbowen@mykolab.com>
2019-07-25 20:22:16 -0500

    Update AffiliationMap.js
    All user names in single quotes.

--- src/app/utils/AffiliationMap.js
+++ src/app/utils/AffiliationMap.js
@@ -12,16 +12,16 @@ const map = {
     steemitdev: 'steemit',
 
     //neoxag
-    neoxian: 'founder',
-    zaku: 'tribe_team',
+    'neoxian': 'founder',
+    'zaku': 'tribe_team',
     'news-today': 'tribe_team',
-    reazuliqbal: 'tribe_team',
-    bala41288: 'tribe_team',
-    thereikiforest: 'pope',
-    r2cornell: 'dragon',
-    scipio: 'dragon',
-    afolwalex: 'dragon',
-    bullionstackers: 'dragon',
+    'reazuliqbal': 'tribe_team',
+    'bala41288': 'tribe_team',
+    'thereikiforest': 'pope',
+    'r2cornell': 'dragon',
+    'scipio': 'dragon',
+    'afolwalex': 'dragon',
+    'bullionstackers': 'dragon',
 };
 
 export default map;
